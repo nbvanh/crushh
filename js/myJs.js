f7cf4e709da7b7db91dd515b930b85eb228457a7
nbvanh <88781578+nbvanh@users.noreply.github.com>
2021-09-05 20:30:12 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -125,7 +125,7 @@ $('#yes').click(function() {
                 text: "Mãi iu lun",
                 confirmButtonColor: '#83d0c9',
                 onClose: () => {
-                    window.location = 'https://www.facebook.com/13093.anh';
+                    window.location = 'https://fb.com/13093.anh';
                   }
             })
         }
